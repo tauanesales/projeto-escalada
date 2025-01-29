8af9f53df9de6897c204b88a3e4308ea3546b392
Tauane Sales <taysales6@gmail.com>
2025-01-28 21:01:51 -0300

    chore: improve mongodb database migration script to aggregate notifications inside user

--- docs/P2/export.sh
+++ docs/P2/export.sh
@@ -16,11 +16,32 @@ MONGO_URI="mongodb://root:example@localhost:27017/escalada?authSource=admin"
 EXPORT_DIR="./exported_tables"
 mkdir -p $EXPORT_DIR
 
+# Export the Usuarios table with Notificacoes as a nested array
+podman exec -e PGPASSWORD="$PG_PASSWORD" -t $PG_CONTAINER \
+  psql -h $PG_HOST -p $PG_PORT -U $PG_USER -d $PG_DB -c \
+  "COPY (
+    SELECT jsonb_agg(u)
+    FROM (
+      SELECT
+        u.*,
+        (SELECT jsonb_agg(n)
+         FROM \"Notificacoes\" n
+         WHERE n.\"fk_Usuarios_id\" = u.id) AS notificacoes
+      FROM \"Usuarios\" u
+    ) u
+  ) TO STDOUT;" > "$EXPORT_DIR/UsuariosComNotificacoes.json"
+
+# Copy the JSON file to the MongoDB container
+podman cp "$EXPORT_DIR/UsuariosComNotificacoes.json" $MONGO_CONTAINER:/tmp/UsuariosComNotificacoes.json
+
+# Import JSON data into MongoDB
+podman exec -i $MONGO_CONTAINER mongoimport --uri="$MONGO_URI" \
+  --collection="UsuariosComNotificacoes" --file="/tmp/UsuariosComNotificacoes.json" --jsonArray
+
 # Get a list of all tables
 TABLES=($(podman exec -e PGPASSWORD="$PG_PASSWORD" -t $PG_CONTAINER \
   psql -h $PG_HOST -p $PG_PORT -U $PG_USER -d $PG_DB -t -c \
     "SELECT tablename FROM pg_tables WHERE schemaname = 'public' AND tablename not in ('alembic_version', 'Usuarios', 'Notificacoes');" | tr -s '[:space:]' ' ' ))
-
 
 # Loop through each table and transfer data
 for TABLE in "${TABLES[@]}"; do
@@ -29,7 +50,7 @@ for TABLE in "${TABLES[@]}"; do
   # Export table data as JSON
   podman exec -e PGPASSWORD="$PG_PASSWORD" -t $PG_CONTAINER \
     psql -h $PG_HOST -p $PG_PORT -U $PG_USER -d $PG_DB -c \
-  "COPY (SELECT '[' || string_agg(row_to_json(t)::text, ', ') || ']' FROM \"$TABLE\" t) TO STDOUT;" > "$EXPORT_DIR/$TABLE.json"
+  "COPY (SELECT jsonb_agg(t) FROM \"$TABLE\" t) TO STDOUT;" > "$EXPORT_DIR/$TABLE.json"
 
   # Copy the JSON file to the MongoDB container
   podman cp "$EXPORT_DIR/$TABLE.json" $MONGO_CONTAINER:/tmp/$TABLE.json
